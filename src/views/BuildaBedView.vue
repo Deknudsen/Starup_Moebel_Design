e174837d87c861759c1dd7548f1cfc609f81c34f
Denni Knudsen <37368491+Deknudsen@users.noreply.github.com>
2023-06-05 00:21:42 +0200

    added build-a-bed site

--- src/views/BuildaBedView.vue
+++ src/views/BuildaBedView.vue
@@ -1,16 +1,188 @@
 <template>
     <div>
-        <div class="randombox"> build a bed </div>
-    </div>
+        <div class="sengeHeader">
+            <p>
+                Her på siden finder du inspiration til hvordan du gerne vil have din seng til at se ud. 
+            </p>
+        </div>
+        <div class="gallery"> 
+          <div class="imageBox" v-for="part in partsData" :key="part">
+            <div class="partContainer">
+            <router-link :to="{ name: 'produktSide', params:{ id : part.id }}">
+              <div class="cardBox">     
+                <div class="galleryImage">
+                 <img :src="part.imageUrl" :alt="part.name" /> 
+                </div>
+                <div class="overlayContainer">
+                  <div class="overlay"></div>
+                  <p class="galleryImageTitle"> {{ part.title }} </p>
+                </div>
+              </div>
+            </router-link>
+            </div>
+          </div>
+        </div>
+     </div> 
+
 </template>
 
 <script setup>
 
+import { onMounted } from 'vue'
+import useParts from '@/modules/useParts'
+
+
+const { partsData, getPartsData } = useParts()
+
+onMounted(() => {
+    getPartsData()
+})
+
+/* export default {
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          src: "https://cdn.pixabay.com/photo/2018/10/28/12/37/bedroom-3778695_1280.jpg",
+          title: "Image 1",
+        },
+        {
+          id: 2,
+          src: "https://cdn.pixabay.com/photo/2016/12/30/07/55/bedroom-1940169_1280.jpg",
+          title: "Image 2",
+        },
+        {
+          id: 3,
+          src: "https://cdn.pixabay.com/photo/2020/10/18/09/16/bedroom-5664221_1280.jpg",
+          title: "Image 3",
+        },
+        {
+          id: 4,
+          src: "https://cdn.pixabay.com/photo/2021/08/27/01/33/bedroom-6577523_1280.jpg",
+          title: "Image 4",
+        },
+        {
+          id: 5,
+          src: "https://cdn.pixabay.com/photo/2021/11/08/00/30/bedroom-6778193_1280.jpg",
+          title: "Image 5",
+        },
+        {
+          id: 6,
+          src: "https://cdn.pixabay.com/photo/2016/12/30/07/55/bedroom-1940168_1280.jpg",
+          title: "Image 6",
+        },
+        {
+          id: 7,
+          src: "https://cdn.pixabay.com/photo/2019/08/19/13/58/bed-4416515_1280.jpg",
+          title: "Image 7",
+        },
+        {
+          id: 8,
+          src: "https://cdn.pixabay.com/photo/2020/11/24/11/36/bedroom-5772286_1280.jpg",
+          title: "Image 8",
+        },
+        {
+          id: 9,
+          src: "https://cdn.pixabay.com/photo/2016/11/19/13/06/bed-1839183_1280.jpg",
+          title: "Image 9",
+        },
+      ],
+    };
+  },
+}; */
 </script>
 
 <style lang="scss" scoped>
-    .randombox {
-        height: 200px;
-        width: 200px;
+.sengeHeader {
+    height: 200px;
+    width: 100%;
+    font-family: $TitleFont;
+    font-size: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        width: 80%;
     }
+}
+
+.gallery {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+    margin: 70px 10% 130px 10%;
+  }
+
+a:link {
+    text-decoration: none;
+}
+.cardBox {
+    height: 450px;
+    width: 400px;
+    background-color: rgb(240, 240, 240);
+    filter: drop-shadow(5px 5px 4px rgb(132, 115, 103));
+    border-radius: 5px;
+    border-style: solid 3px;
+    border-color: rgb(218, 218, 218);
+}
+.galleryImage {
+    height: 100%;
+    width: auto;
+    background-color: #fff;
+    border-radius: 10px 10px 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+     img {
+        object-fit: cover;
+        width:auto;
+        height: 100%;
+        overflow: hidden;
+    }
+ }
+.partContainer {
+  position: relative;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: $Tertiary;
+}
+
+.partContainer:hover .overlay {
+  opacity: 0.7;
+}
+
+.galleryImageTitle {
+  color: $Black;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  opacity: 0;
+  transition: .5s ease;
+}
+
+.partContainer:hover .galleryImageTitle {
+  opacity: 1;
+}
+
 </style>
